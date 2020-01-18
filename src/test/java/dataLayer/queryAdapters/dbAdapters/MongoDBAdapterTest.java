5777e25e820b9f01cad4e41f5e78b741f437242f
Unknown <yossilan@post.bgu.ac.il>
2020-01-18 15:56:33 +0200

    Added tests
    Added new tests.

--- src/test/java/dataLayer/queryAdapters/dbAdapters/MongoDBAdapterTest.java
+++ src/test/java/dataLayer/queryAdapters/dbAdapters/MongoDBAdapterTest.java
@@ -3,18 +3,21 @@ package dataLayer.queryAdapters.dbAdapters;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import dataLayer.configReader.Conf;
-import dataLayer.queryAdapters.crud.Eq;
+import dataLayer.queryAdapters.crud.*;
 import org.bson.Document;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+
+import javax.validation.constraints.AssertTrue;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MongoDBAdapterTest
@@ -36,7 +39,7 @@ class MongoDBAdapterTest
 							new Document("name", "Yossi")
 									.append("age", 22)
 									.append("phoneNumber", "0587158627")
-									.append("emailAddress", "Alice@post.bgu.ac.il"),
+									.append("emailAddress", "yossilan@post.bgu.ac.il"),
 							new Document("name", "Karin")
 									.append("age", 26)
 									.append("phoneNumber", "0504563434")
@@ -77,32 +80,168 @@ class MongoDBAdapterTest
 	void testExecuteEq()
 	{
 		List<Map<String, Object>> result = mongoDBAdapter.revealQuery(Eq.eq("Person", "name", "Roy"));
-		System.out.println(result);
+		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
+				result.get(0).get("age").equals(27) &&
+				result.get(0).get("phoneNumber").equals("0546815181") &&
+				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
+		assertTrue(hasRoy, "Roy's name == Roy");
+
+		result = mongoDBAdapter.revealQuery(Eq.eq("Person", "name", "Nobody"));
+		assertTrue(result.isEmpty(), "There is no person named Nobody");
 	}
 
 	@Test
 	void testExecuteNe()
 	{
+		List<Map<String, Object>> result = mongoDBAdapter.revealQuery(Ne.ne("Person", "name", "Roy"));
+		boolean hasYossi = result.get(0).get("name").equals("Yossi") &&
+				result.get(0).get("age").equals(22) &&
+				result.get(0).get("phoneNumber").equals("0587158627") &&
+				result.get(0).get("emailAddress").equals("yossilan@post.bgu.ac.il");
+		assertTrue(hasYossi, "Yossi's name != Roy.");
+		boolean hasKarin = result.get(1).get("name").equals("Karin") &&
+				result.get(1).get("age").equals(26) &&
+				result.get(1).get("phoneNumber").equals("0504563434") &&
+				result.get(1).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's name != Roy.");
+
+		result = mongoDBAdapter.revealQuery(Ne.ne("Person", "name", "Nobody"));
+		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
+				result.get(0).get("age").equals(27) &&
+				result.get(0).get("phoneNumber").equals("0546815181") &&
+				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
+		assertTrue(hasRoy, "Roy's name != Nobody.");
+		hasYossi = result.get(1).get("name").equals("Yossi") &&
+				result.get(1).get("age").equals(22) &&
+				result.get(1).get("phoneNumber").equals("0587158627") &&
+				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
+		assertTrue(hasYossi, "Yossi's name != Nobody.");
+		hasKarin = result.get(2).get("name").equals("Karin") &&
+				result.get(2).get("age").equals(26) &&
+				result.get(2).get("phoneNumber").equals("0504563434") &&
+				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's name != Nobody.");
 	}
 
 	@Test
 	void testExecuteGt()
 	{
+		List<Map<String, Object>> result = mongoDBAdapter.revealQuery(Gt.gt("Person", "age", 18));
+		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
+				result.get(0).get("age").equals(27) &&
+				result.get(0).get("phoneNumber").equals("0546815181") &&
+				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
+		assertTrue(hasRoy, "Roy's age is > 18.");
+		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
+				result.get(1).get("age").equals(22) &&
+				result.get(1).get("phoneNumber").equals("0587158627") &&
+				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
+		assertTrue(hasYossi, "Yossi's age is > 18.");
+		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
+				result.get(2).get("age").equals(26) &&
+				result.get(2).get("phoneNumber").equals("0504563434") &&
+				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's age is > 18.");
+
+		result = mongoDBAdapter.revealQuery(Gt.gt("Person", "age", 30));
+		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are <= 30.");
 	}
 
 	@Test
 	void testExecuteLt()
 	{
+		List<Map<String, Object>> result = mongoDBAdapter.revealQuery(Lt.lt("Person", "age", 30));
+		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
+				result.get(0).get("age").equals(27) &&
+				result.get(0).get("phoneNumber").equals("0546815181") &&
+				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
+		assertTrue(hasRoy, "Roy's age is < 30.");
+		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
+				result.get(1).get("age").equals(22) &&
+				result.get(1).get("phoneNumber").equals("0587158627") &&
+				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
+		assertTrue(hasYossi, "Yossi's age is < 30.");
+		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
+				result.get(2).get("age").equals(26) &&
+				result.get(2).get("phoneNumber").equals("0504563434") &&
+				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's age is < 30.");
+
+		result = mongoDBAdapter.revealQuery(Lt.lt("Person", "age", 18));
+		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are >= 18.");
 	}
 
 	@Test
 	void testExecuteGte()
 	{
+		List<Map<String, Object>> result = mongoDBAdapter.revealQuery(Gte.gte("Person", "age", 18));
+		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
+				result.get(0).get("age").equals(27) &&
+				result.get(0).get("phoneNumber").equals("0546815181") &&
+				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
+		assertTrue(hasRoy, "Roy's age is >= 18.");
+		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
+				result.get(1).get("age").equals(22) &&
+				result.get(1).get("phoneNumber").equals("0587158627") &&
+				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
+		assertTrue(hasYossi, "Yossi's age is >= 18.");
+		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
+				result.get(2).get("age").equals(26) &&
+				result.get(2).get("phoneNumber").equals("0504563434") &&
+				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's age is >= 18.");
+
+
+		result = mongoDBAdapter.revealQuery(Gte.gte("Person", "age", 26));
+		hasRoy = result.get(0).get("name").equals("Roy") &&
+				result.get(0).get("age").equals(27) &&
+				result.get(0).get("phoneNumber").equals("0546815181") &&
+				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
+		assertTrue(hasRoy, "Roy's age is >= 26.");
+		hasKarin = result.get(1).get("name").equals("Karin") &&
+				result.get(1).get("age").equals(26) &&
+				result.get(1).get("phoneNumber").equals("0504563434") &&
+				result.get(1).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's age is >= 26.");
+
+		result = mongoDBAdapter.revealQuery(Gte.gte("Person", "age", 30));
+		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are < 30.");
 	}
 
 	@Test
 	void testExecuteLte()
 	{
+		List<Map<String, Object>> result = mongoDBAdapter.revealQuery(Lte.lte("Person", "age", 30));
+		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
+				result.get(0).get("age").equals(27) &&
+				result.get(0).get("phoneNumber").equals("0546815181") &&
+				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
+		assertTrue(hasRoy, "Roy's age is <= 30.");
+		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
+				result.get(1).get("age").equals(22) &&
+				result.get(1).get("phoneNumber").equals("0587158627") &&
+				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
+		assertTrue(hasYossi, "Yossi's age is <= 30.");
+		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
+				result.get(2).get("age").equals(26) &&
+				result.get(2).get("phoneNumber").equals("0504563434") &&
+				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's age is <= 30.");
+
+		result = mongoDBAdapter.revealQuery(Lte.lte("Person", "age", 26));
+		hasYossi = result.get(0).get("name").equals("Yossi") &&
+				result.get(0).get("age").equals(22) &&
+				result.get(0).get("phoneNumber").equals("0587158627") &&
+				result.get(0).get("emailAddress").equals("yossilan@post.bgu.ac.il");
+		assertTrue(hasYossi, "Yossi's age is <= 26.");
+		hasKarin = result.get(1).get("name").equals("Karin") &&
+				result.get(1).get("age").equals(26) &&
+				result.get(1).get("phoneNumber").equals("0504563434") &&
+				result.get(1).get("emailAddress").equals("davidz@post.bgu.ac.il");
+		assertTrue(hasKarin, "Karin's age is <= 26.");
+
+		result = mongoDBAdapter.revealQuery(Lte.lte("Person", "age", 18));
+		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are > 18.");
 	}
 
 	@Test
