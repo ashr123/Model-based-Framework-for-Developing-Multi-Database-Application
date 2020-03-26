d0a4e9db5468219badf80b955b3125cab96fd043
Yossi Landa <yossilan@post.bgu.ac.il>
2020-03-26 13:45:24 +0200

    (no branch): Auto stash before checking out "HEAD"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -81,6 +81,10 @@ import org.neo4j.driver.v1.Session;
 
 import java.util.Properties;
 
+
+import java.util.Properties;
+>>>>>>> Stashed changes
+
 //import static org.neo4j.driver.Values.parameters;
 
 public class Main
@@ -88,6 +92,7 @@ public class Main
 	public static void main(String... args)
 	{
 //		try (HelloWorldExample greeter = new HelloWorldExample("bolt://localhost:7687", "neo4j", "neo4j1"))
+<<<<<<< Updated upstream
 //		{
 //			greeter.printGreeting("hello, world");
 //		}
@@ -130,6 +135,41 @@ public class Main
 		{
 			r_dbAccess.close();
 		}
+	}
+
+	private static class HelloWorldExample2 implements AutoCloseable
+	{
+		private final IDBAccess r_dbAccess;
+
+		public HelloWorldExample2(String uri, String user, String password)
+		{
+
+			Properties props = new Properties();
+			props.setProperty(DBProperties.SERVER_ROOT_URI, uri);
+			r_dbAccess = DBAccessFactory.createDBAccess(DBType.REMOTE, props, AuthTokens.basic(user, password));
+		}
+
+		public void query()
+		{
+			JcNode movie = new JcNode("movie");
+			JcQuery query = new JcQuery();
+			query.setClauses(new IClause[]{
+					MATCH.node(movie).label("Movie"),
+					RETURN.value(movie.property("title"))
+			});
+
+			JcQueryResult result = r_dbAccess.execute(query);
+
+			System.out.println("DB errors: " + result.getDBErrors() + "\nGeneral errors: " + result.getGeneralErrors());
+			System.out.println("RESULT!!!!!\n"+result.resultOf(movie.property("title")));
+		}
+
+		@Override
+		public void close()
+		{
+			r_dbAccess.close();
+		}
+>>>>>>> Stashed changes
 	}
 
 	private static class HelloWorldExample implements AutoCloseable
