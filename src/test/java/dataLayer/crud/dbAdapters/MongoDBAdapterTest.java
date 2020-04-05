99477a0a5d45f0837e3a397451e305d6b71a7924
Yossi Landa <yossilan@post.bgu.ac.il>
2020-04-05 19:17:10 +0300

    Tests for MongoDBAdapter.

--- src/test/java/dataLayer/crud/dbAdapters/MongoDBAdapterTest.java
+++ src/test/java/dataLayer/crud/dbAdapters/MongoDBAdapterTest.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoClients;
 import dataLayer.configReader.Conf;
 import dataLayer.configReader.Entity;
 import dataLayer.crud.filters.CreateMany;
+import dataLayer.crud.filters.Ne;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,11 @@ import java.util.Set;
 import static dataLayer.crud.Read.read;
 import static dataLayer.crud.filters.And.and;
 import static dataLayer.crud.filters.Eq.eq;
+import static dataLayer.crud.filters.Gt.gt;
 import static dataLayer.crud.filters.Gte.gte;
+import static dataLayer.crud.filters.Lt.lt;
 import static dataLayer.crud.filters.Lte.lte;
+import static dataLayer.crud.filters.Ne.ne;
 import static dataLayer.crud.filters.Or.or;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -108,165 +112,81 @@ class MongoDBAdapterTest
 	void testExecuteEq()
 	{
 		assertEquals(Set.of(roy),
-				read(eq("Person", "name", "Roy")));
+				read(eq("Person", "name", "Roy")),
+				"Should return person named Roy.");
 
-		assertTrue(read(eq("Person", "name", "Nobody")).isEmpty(), "Should be empty!");
+		assertEquals(Set.of(),
+				read(eq("Person", "name", "Nobody")),
+				"There is no person named Nobody");
 	}
 
-//	@Test
-//	void testExecuteNe()
-//	{
-//		List<Map<String, Object>> result = read(Ne.ne("Person", "name", "Roy"));
-//		System.out.println(result);
-//		boolean hasYossi = result.get(0).get("name").equals("Yossi") &&
-//				result.get(0).get("age").equals(22) &&
-//				result.get(0).get("phoneNumber").equals("0587158627") &&
-//				result.get(0).get("emailAddress").equals("yossilan@post.bgu.ac.il");
-//		assertTrue(hasYossi, "Yossi's name != Roy.");
-//		boolean hasKarin = result.get(1).get("name").equals("Karin") &&
-//				result.get(1).get("age").equals(26) &&
-//				result.get(1).get("phoneNumber").equals("0504563434") &&
-//				result.get(1).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's name != Roy.");
-//
-//		result = read(Ne.ne("Person", "name", "Nobody"));
-//		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
-//				result.get(0).get("age").equals(27) &&
-//				result.get(0).get("phoneNumber").equals("0546815181") &&
-//				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
-//		assertTrue(hasRoy, "Roy's name != Nobody.");
-//		hasYossi = result.get(1).get("name").equals("Yossi") &&
-//				result.get(1).get("age").equals(22) &&
-//				result.get(1).get("phoneNumber").equals("0587158627") &&
-//				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
-//		assertTrue(hasYossi, "Yossi's name != Nobody.");
-//		hasKarin = result.get(2).get("name").equals("Karin") &&
-//				result.get(2).get("age").equals(26) &&
-//				result.get(2).get("phoneNumber").equals("0504563434") &&
-//				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's name != Nobody.");
-//	}
-//
-//	@Test
-//	void testExecuteGt()
-//	{
-//		List<Map<String, Object>> result = read(gt("Person", "age", 18));
-//		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
-//				result.get(0).get("age").equals(27) &&
-//				result.get(0).get("phoneNumber").equals("0546815181") &&
-//				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
-//		assertTrue(hasRoy, "Roy's age is > 18.");
-//		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
-//				result.get(1).get("age").equals(22) &&
-//				result.get(1).get("phoneNumber").equals("0587158627") &&
-//				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
-//		assertTrue(hasYossi, "Yossi's age is > 18.");
-//		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
-//				result.get(2).get("age").equals(26) &&
-//				result.get(2).get("phoneNumber").equals("0504563434") &&
-//				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's age is > 18.");
-//
-//		result = read(gt("Person", "age", 30));
-//		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are <= 30.");
-//	}
-//
-//	@Test
-//	void testExecuteLt()
-//	{
-//		List<Map<String, Object>> result = read(Lt.lt("Person", "age", 30));
-//		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
-//				result.get(0).get("age").equals(27) &&
-//				result.get(0).get("phoneNumber").equals("0546815181") &&
-//				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
-//		assertTrue(hasRoy, "Roy's age is < 30.");
-//		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
-//				result.get(1).get("age").equals(22) &&
-//				result.get(1).get("phoneNumber").equals("0587158627") &&
-//				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
-//		assertTrue(hasYossi, "Yossi's age is < 30.");
-//		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
-//				result.get(2).get("age").equals(26) &&
-//				result.get(2).get("phoneNumber").equals("0504563434") &&
-//				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's age is < 30.");
-//
-//		result = read(Lt.lt("Person", "age", 18));
-//		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are >= 18.");
-//	}
-//
-//	@Test
-//	void testExecuteGte()
-//	{
-//		List<Map<String, Object>> result = read(gte("Person", "age", 18));
-//		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
-//				result.get(0).get("age").equals(27) &&
-//				result.get(0).get("phoneNumber").equals("0546815181") &&
-//				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
-//		assertTrue(hasRoy, "Roy's age is >= 18.");
-//		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
-//				result.get(1).get("age").equals(22) &&
-//				result.get(1).get("phoneNumber").equals("0587158627") &&
-//				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
-//		assertTrue(hasYossi, "Yossi's age is >= 18.");
-//		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
-//				result.get(2).get("age").equals(26) &&
-//				result.get(2).get("phoneNumber").equals("0504563434") &&
-//				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's age is >= 18.");
-//
-//
-//		result = read(gte("Person", "age", 26));
-//		hasRoy = result.get(0).get("name").equals("Roy") &&
-//				result.get(0).get("age").equals(27) &&
-//				result.get(0).get("phoneNumber").equals("0546815181") &&
-//				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
-//		assertTrue(hasRoy, "Roy's age is >= 26.");
-//		hasKarin = result.get(1).get("name").equals("Karin") &&
-//				result.get(1).get("age").equals(26) &&
-//				result.get(1).get("phoneNumber").equals("0504563434") &&
-//				result.get(1).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's age is >= 26.");
-//
-//		result = read(gte("Person", "age", 30));
-//		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are < 30.");
-//	}
-//
-//	@Test
-//	void testExecuteLte()
-//	{
-//		List<Map<String, Object>> result = read(lte("Person", "age", 30));
-//		boolean hasRoy = result.get(0).get("name").equals("Roy") &&
-//				result.get(0).get("age").equals(27) &&
-//				result.get(0).get("phoneNumber").equals("0546815181") &&
-//				result.get(0).get("emailAddress").equals("ashr@post.bgu.ac.il");
-//		assertTrue(hasRoy, "Roy's age is <= 30.");
-//		boolean hasYossi = result.get(1).get("name").equals("Yossi") &&
-//				result.get(1).get("age").equals(22) &&
-//				result.get(1).get("phoneNumber").equals("0587158627") &&
-//				result.get(1).get("emailAddress").equals("yossilan@post.bgu.ac.il");
-//		assertTrue(hasYossi, "Yossi's age is <= 30.");
-//		boolean hasKarin = result.get(2).get("name").equals("Karin") &&
-//				result.get(2).get("age").equals(26) &&
-//				result.get(2).get("phoneNumber").equals("0504563434") &&
-//				result.get(2).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's age is <= 30.");
-//
-//		result = read(lte("Person", "age", 26));
-//		hasYossi = result.get(0).get("name").equals("Yossi") &&
-//				result.get(0).get("age").equals(22) &&
-//				result.get(0).get("phoneNumber").equals("0587158627") &&
-//				result.get(0).get("emailAddress").equals("yossilan@post.bgu.ac.il");
-//		assertTrue(hasYossi, "Yossi's age is <= 26.");
-//		hasKarin = result.get(1).get("name").equals("Karin") &&
-//				result.get(1).get("age").equals(26) &&
-//				result.get(1).get("phoneNumber").equals("0504563434") &&
-//				result.get(1).get("emailAddress").equals("davidz@post.bgu.ac.il");
-//		assertTrue(hasKarin, "Karin's age is <= 26.");
-//
-//		result = read(lte("Person", "age", 18));
-//		assertTrue(result.isEmpty(), "Result should be empty all of the people ages are > 18.");
-//	}
+	@Test
+	void testExecuteNe()
+	{
+		assertEquals(Set.of(yossi, karin),
+				read(ne("Person", "name", "Roy")),
+				"Should return everyone except Roy.");
+
+		assertEquals(Set.of(roy, yossi, karin),
+				read(ne("Person", "name", "Nobody")),
+				"Should return everyone since everyone are not named Nobody.");
+	}
+
+	@Test
+	void testExecuteGt()
+	{
+		assertEquals(Set.of(roy, yossi, karin),
+				read(gt("Person", "age", 18)),
+				"Should return everyone since all are above the age of 18.");
+
+		assertEquals(Set.of(),
+				read(gt("Person", "age", 30)),
+				"Result should be empty all of the people ages are <= 30.");
+	}
+
+	@Test
+	void testExecuteLt()
+	{
+		assertEquals(Set.of(roy, yossi, karin),
+				read(lt("Person", "age", 30)),
+				"Should return everyone since all are under the age of 30.");
+
+		assertEquals(Set.of(),
+				read(lt("Person", "age", 18)),
+				"Result should be empty all of the people ages are >= 18.");
+	}
+
+	@Test
+	void testExecuteGte()
+	{
+		assertEquals(Set.of(roy, yossi, karin),
+				read(gte("Person", "age", 18)),
+				"Should return everyone since all are over the age of 18.");
+
+		assertEquals(Set.of(roy, karin),
+				read(gte("Person", "age", 26)),
+				"Only Roy and Karin are above/equal to 26.");
+
+		assertEquals(Set.of(),
+				read(gte("Person", "age", 30)),
+				"Result should be empty all of the people ages are < 30.");
+	}
+
+	@Test
+	void testExecuteLte()
+	{
+		assertEquals(Set.of(roy, yossi, karin),
+				read(lte("Person", "age", 30)),
+				"Should return everyone since all are under the age of 30.");
+
+		assertEquals(Set.of(yossi, karin),
+				read(lte("Person", "age", 26)),
+				"Only Yossi and Karin are under/equal to 26.");
+
+		assertEquals(Set.of(),
+				read(lte("Person", "age", 18)),
+				"Result should be empty all of the people ages are > 18.");
+	}
 
 	@Test
 	void testExecuteAnd()
@@ -297,12 +217,4 @@ class MongoDBAdapterTest
 										eq("Person", "phoneNumber", "0587158627"),
 										eq("Person", "name", "Yossi"))))));
 	}
-
-//	@Test
-//	void testExecuteTest()
-//	{
-//		System.out.println(read(and(
-//				gt("Person", "age", 30),
-//				eq("Person", "name", "Yossi"))));
-//	}
 }
