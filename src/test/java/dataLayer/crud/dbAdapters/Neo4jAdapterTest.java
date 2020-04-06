6ffce1499c71222d92a7cd3c807b1cfd27a540d4
ashr123 <roy.ash456@gmail.com>
2020-04-06 22:33:14 +0300

    Before testing Neo4JAdapter

---
+++ src/test/java/dataLayer/crud/dbAdapters/Neo4jAdapterTest.java
@@ -0,0 +1,69 @@
+package dataLayer.crud.dbAdapters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Neo4jAdapterTest
+{
+
+	@Test
+	void revealQuery()
+	{
+	}
+
+	@Test
+	void testRevealQuery()
+	{
+	}
+
+	@Test
+	void execute()
+	{
+	}
+
+	@Test
+	void testExecute()
+	{
+	}
+
+	@Test
+	void executeCreate()
+	{
+	}
+
+	@Test
+	void testExecuteCreate()
+	{
+	}
+
+	@Test
+	void testExecute1()
+	{
+	}
+
+	@Test
+	void testExecute2()
+	{
+	}
+
+	@Test
+	void testExecute3()
+	{
+	}
+
+	@Test
+	void testExecute4()
+	{
+	}
+
+	@Test
+	void testExecute5()
+	{
+	}
+
+	@Test
+	void testExecute6()
+	{
+	}
+}
