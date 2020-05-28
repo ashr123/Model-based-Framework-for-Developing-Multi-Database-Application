835bbf8c80d78d6c367d071e5c2d648028105f06
Roy Ash <roy.ash456@gmail.com>
2020-05-28 13:50:47 +0300

    sqlite

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
 import dataLayer.readers.Reader;
+import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
 
 import java.io.IOException;
 
 public class Main
 {
-	public static void main(String... args) throws IOException, NoSuchMethodException
+	public static void main(String... args) throws IOException
 	{
 		Reader.loadConfAndSchema(Main.class.getResource("/configurations/configurationMongoDB.json"),
 				Main.class.getResource("/schemas/Schema.json"));
-		System.out.println(DSL.select(DSL.field("name")).from("Person").getSQL());
+		try (DSLContext connection = DSL.using("jdbc:sqlite:src/main/resources/sqliteDBs/test.db"))
+		{
+			connection.createTableIfNotExists("testTable").column("ts", SQLDataType.INTEGER).execute();
+		}
+//		System.out.println(DSL.select(DSL.field("name")).from("Person").getSQL());
 	}
 }
